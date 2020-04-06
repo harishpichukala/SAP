ce0abf55f72d28174011703bd01a886a24ce48a6
harish pichukala <harish.pichukala93@gmail.com>
2020-04-05 18:22:06 -0700

    Update DB_Report.sql

--- DB_Report.sql
+++ DB_Report.sql
@@ -3,4 +3,4 @@ from m_backup_catalog_files f, m_backup_catalog c where
 f.source_type_name in ('volume','topology') and f.service_type_name in ('indexserver','nameserver','xsengine')
 and c.backup_id = f.backup_id
 and c.entry_type_name in ('complete data backup','incremental data backup')
-order by f.backup_id desc) group by id,start_time,end;
+order by f.backup_id desc) group by id,start_time,end_time,status;
